91632d9768c198e1465db0220ab8d7f988402630
simoni savani <simonisavani39@gmail.com>
2025-02-10 18:20:25 +0530

    Initial commit

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,31 @@
+// const jwt = require("jsonwebtoken");
+
+// const verifyToken = async (request, h) => {
+//   try {
+//     const token = request.headers.authorization.split(" ")[1];
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     request.user = decoded;
+//     return h.continue;
+//   } catch (error) {
+//     return h.response({ message: "Unauthorized" }).code(401);
+//   }
+// };
+
+// module.exports = { verifyToken };
 const jwt = require("jsonwebtoken");
 
-const verifyToken = async (request, h) => {
+module.exports = async (request, h) => {
+  const token = request.headers.authorization;
+
+  if (!token) {
+    return h.response({ error: "Token missing" }).code(401).takeover();
+  }
+
   try {
-    const token = request.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     request.user = decoded;
     return h.continue;
   } catch (error) {
-    return h.response({ message: "Unauthorized" }).code(401);
+    return h.response({ error: "Invalid token" }).code(401).takeover();
   }
 };
-
-module.exports = { verifyToken };
